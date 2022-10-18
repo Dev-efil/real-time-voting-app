5e6e2ab6443bc499d6ce726a42bafddaea2b935c
Thanushkanth Shanmuganathan <35372972+Dev-efil@users.noreply.github.com>
2022-10-18 22:32:08 +0530

    connect server and client, integrate voting

--- server/server.js
+++ server/server.js
@@ -13,4 +13,48 @@ app.get('/test', (req, res) => {
     return res.status(200).json({ message: "Successfully" });
 });
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+const voteList = {
+    '0' : {
+        SriLanka : 0,
+        Namibia : 0
+    },
+    '1' : {
+        UAE : 0,
+        Netherlands : 0
+    },
+    '2' : {
+        WestIndies : 0,
+        Scotland : 0
+    }
+}
+
+// Socket.io code
+const io = socket(server, {
+    cors: {
+        origin: 'http://localhost:3000'
+    },
+});
+
+io.on('connection', function (socket) {
+    console.log(`Made a Socket connection 🔌 : ${socket.id}`);
+    // Register an event to console the data which coming from Client.
+    socket.on('vote', (vote) => {
+        const element = Number(Object.keys(vote)[0]);
+        if(voteList[element]) {
+            if(Object.keys(vote[0])[0] === '0') {
+                voteList[0].SriLanka +=1;
+            }
+            else if(Object.keys(vote[0])[0] === '1') {
+                voteList[0].Namibia +=1;
+            }
+        }
+        else if (voteList[vote[1]]) {
+            console.log('something');
+        }
+        // broadcast the event to all connected users
+        io.emit('broadcast', voteList);
+    });
+    io.emit('broadcast', voteList);
+});
--- voting-app/src/components/vote/vote.js
+++ voting-app/src/components/vote/vote.js
@@ -1,7 +1,43 @@
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:5000');
+
 function Vote() {
+    const [result, setResult] = useState();
+
+    useEffect(() => {
+        socket.on('broadcast', (voteList) => {
+            const res = voteList;
+            setResult(res);
+        })
+    }, [])
+
+    const voteSriLanka = () => {
+        const vote = 0;
+        const result = {
+            '0': {
+                '0': vote
+            }
+        }
+        socket.emit('vote', result);
+    }
+    const voteNamibia = () => {
+        const vote = 1;
+        const result = {
+            '0': {
+                '1': vote
+            }
+        }
+        socket.emit('vote', result);
+    }
     return (
         <div>
-            <p>vote</p>
+            <button onClick={voteSriLanka}>Sri lanka</button>
+            <button onClick={voteNamibia}>Namibia</button>
+            <div>
+                <code>{JSON.stringify(result)}</code>
+            </div>
         </div>
     )
 }
